328a5582ca980908433a92ee9d73a29aabc9a38a
vipul-khedekar <3kvipul@gmail.com>
2022-09-04 00:02:05 +0530

    Update: Custom color scheme.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkGold: "#8e793e",
+        gold: "#ad974f",
+        black: "#000000",
+        semiLightBlack: "#231f20",
+        semiDarkWhite: "#eaeaea",
+        white: "#ffffff",
+      },
+    },
   },
   plugins: [],
 };
