11eab74a93a73032508e5e8f76f5752ae2c8fb81
vipul-khedekar <3kvipul@gmail.com>
2022-09-03 20:31:55 +0530

    Update: Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import Pages from "./pages/Pages";
 
 function App() {
-  return (
-    <div>
-      <Pages />
-    </div>
-  );
+  return <Pages />;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+import "./main.css";
 import App from "./App";
 
 ReactDOM.createRoot(document.querySelector(`#root`) as HTMLElement).render(
