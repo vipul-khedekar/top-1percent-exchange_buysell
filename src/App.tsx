19e51677719586af5bff86a92f10ad3a653e89c8
vipul-khedekar <3kvipul@gmail.com>
2022-09-03 23:58:00 +0530

    Update: Styled. Added links and icons.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import Pages from "./pages/Pages";
 
 function App() {
-  return <Pages />;
+  return (
+    <div className="m-5">
+      <Pages />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,32 @@
+import { IoCartOutline, IoDiamondSharp } from "react-icons/io5";
+
 import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <header>
-      <NavLink to={"/store/buy"}>Buy</NavLink>
-      <NavLink to={"/store/sell"}>Sell</NavLink>
+    <header className="h-8 flex justify-between items-center gap-20 my-5 relative">
+      <NavLink
+        className="flex justify-center items-center gap-2 flex-auto text-xl hover:text-2xl"
+        to={"/"}
+      >
+        <IoDiamondSharp />
+        <p>Home</p>
+      </NavLink>
+      <NavLink
+        className="flex justify-center items-center flex-auto text-xl hover:text-2xl"
+        to={"/store/buy"}
+      >
+        <p>Buy</p>
+      </NavLink>
+      <NavLink
+        className="flex justify-center items-center flex-auto text-xl hover:text-2xl"
+        to={"/store/sell"}
+      >
+        <p>Sell</p>
+      </NavLink>
+      <div className="flex justify-center items-center flex-auto text-3xl hover:text-4xl">
+        <IoCartOutline />
+      </div>
     </header>
   );
 }
