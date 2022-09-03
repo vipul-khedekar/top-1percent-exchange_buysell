18e3dcd5ef3848072983124da8a93d675019f18b
vipul-khedekar <3kvipul@gmail.com>
2022-09-03 19:35:47 +0530

    Update: Imported bootstrap. Fix: Cleared boilerplate code.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import App from "./App";
+
+ReactDOM.createRoot(document.querySelector(`#root`) as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
