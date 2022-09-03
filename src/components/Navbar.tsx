01a348284a1a787482e2f3792800086f3a1bdc06
vipul-khedekar <3kvipul@gmail.com>
2022-09-04 00:08:30 +0530

    Fix: Header titles don't twitch and stutter .

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,27 +6,33 @@ function Navbar() {
   return (
     <header className="h-8 flex justify-between items-center gap-20 my-5 relative">
       <NavLink
-        className="flex justify-center items-center gap-2 flex-auto text-xl hover:text-2xl"
+        className="w-1 flex justify-center items-center gap-2 flex-auto text-xl hover:text-2xl"
         to={"/"}
       >
         <IoDiamondSharp />
         <p>Home</p>
       </NavLink>
+
       <NavLink
-        className="flex justify-center items-center flex-auto text-xl hover:text-2xl"
+        className="w-1 flex justify-center items-center flex-auto text-xl hover:text-2xl"
         to={"/store/buy"}
       >
         <p>Buy</p>
       </NavLink>
+
       <NavLink
-        className="flex justify-center items-center flex-auto text-xl hover:text-2xl"
+        className="w-1 flex justify-center items-center flex-auto text-xl hover:text-2xl"
         to={"/store/sell"}
       >
         <p>Sell</p>
       </NavLink>
-      <div className="flex justify-center items-center flex-auto text-3xl hover:text-4xl">
+
+      <NavLink
+        className="w-1 flex justify-center items-center flex-auto text-3xl hover:text-4xl"
+        to={"/cart"}
+      >
         <IoCartOutline />
-      </div>
+      </NavLink>
     </header>
   );
 }
