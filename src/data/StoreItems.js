379ab941292459c13f483140cec8c7b408ddea6f
vipul-khedekar <3kvipul@gmail.com>
2022-09-04 17:09:25 +0530

    New: Data filled to display store item details.

---
+++ src/data/StoreItems.js
@@ -0,0 +1,386 @@
+export const StoreItems = [
+  {
+    id: 1,
+    category: `cars`,
+    owner: {
+      name: `Neil Legstrong`,
+      country: `United States`,
+    },
+    item: {
+      name: `Porsche 912`,
+      price: `4,701,093`,
+      year: 1966,
+      place: `Glendale, United States`,
+      about: `Vintage Porsche.`,
+    },
+    images: {
+      main: `https://img.jamesedition.com/listing_images/2022/05/13/15/24/47/dc2184df-a65e-41ea-b172-75354e734bf6/je/1100xxs.jpg`,
+      secondary: [
+        `https://img.jamesedition.com/listing_images/2022/05/13/15/24/47/722e25ab-8e2b-4212-94b1-7a3ff893674e/je/1100xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2022/05/13/15/24/47/cc6b84b9-ba7f-4de0-aa8f-7867678a1159/je/1100xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2022/05/13/15/24/47/35b589ed-c1d0-44b8-b0ca-f8ecba82df7e/je/1100xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2022/05/13/15/24/47/679b0ebc-eb04-40e7-a7b3-5fa12e049ffb/je/1100xxs.jpg`,
+      ],
+    },
+  },
+  {
+    id: 2,
+    category: `bikes`,
+    owner: {
+      name: `Virat Holi`,
+      country: `United States`,
+    },
+    item: {
+      name: `DUCATI Multistrada ABS`,
+      price: `753,290`,
+      year: 2012,
+      place: ` Buffalo, NY, United States`,
+      about: `We have a full line of Ducati Motorcycles, Parts, Accessories & Apparels (Ducati Performance). Great selection of mildly used Ducati Motorcycles in showroom as well.`,
+    },
+    images: {
+      main: `https://img.jamesedition.com/listing_images/2018/12/21/16/08/52/e5c13467-2fb9-463e-8bc0-03539943474d/je/2000xxs.jpg`,
+      secondary: [
+        `https://img.jamesedition.com/listing_images/2018/12/21/16/08/52/6a2c1a92-947f-4591-ae69-e9164a4a6d5d/je/2000xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2018/12/21/16/08/53/ef32ce4b-946d-4dfc-ad7d-1ab2bdccd949/je/2000xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2018/12/21/16/08/52/4adb00cd-0243-418a-a712-ef3629a75bae/je/2000xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2018/12/21/16/08/52/43b58458-5a10-4cb9-97ab-580e0a5e3f18/je/2000xxs.jpg`,
+      ],
+    },
+  },
+  {
+    id: 3,
+    category: `cars`,
+    owner: {
+      name: `Nikola Edison`,
+      country: `Italy`,
+    },
+    item: {
+      name: `Ford Thunderbird`,
+      price: `3,185,129`,
+      year: 1956,
+      place: `Brescia, Italy`,
+      about: `A nicely restored '56 bird with some Ford Exec history, plenty of options and a nice looking example which has won many shows including a few best of show badges. A convertible, hardtop and nice history come with this car which is raring to take flight and make more history and memories.`,
+    },
+    images: {
+      main: `https://img.jamesedition.com/listing_images/2021/01/13/17/50/29/df47c92d-c327-4688-998a-eefe711dbfa6/je/1100xxs.jpg`,
+      secondary: [
+        `https://img.jamesedition.com/listing_images/2021/01/13/17/50/34/1dfdfb70-334c-489d-80f6-943fbe9e6dcb/je/1100xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2021/01/13/17/50/34/6f26e679-4e45-4b3f-bbcf-c6dfe3a1f69f/je/1100xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2021/01/13/17/50/29/1de711a8-1a27-4133-a617-a5664200e8a7/je/1100xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2021/01/13/17/50/34/e4bd7ad5-8cb7-434f-a400-5fcd72f82770/je/1100xxs.jpg`,
+      ],
+    },
+  },
+  {
+    id: 4,
+    category: `collectibles`,
+    owner: {
+      name: `Joe Mama`,
+      country: `Australia`,
+    },
+    item: {
+      name: `Hermes Kelly II Sellier 28 in Veau Epsom Leather`,
+      price: `1,115,513.77`,
+      year: 2008,
+      place: `Melbourne, Victoria, Australia`,
+      about: `Sophisticated, chic, and dateless the Kelly II will compliment your outfit on any occasion. This 28 cm Hermes Kelly II Sellier in beautiful Veau Epsom leather, an embossed leather that is recognised for its durability. The colour is Noir 89 (Black) and it is complemented beautifully with the application of gold hardware.`,
+    },
+    images: {
+      main: `https://img.jamesedition.com/listing_images/2021/09/21/17/16/55/c53fe9fb-2f15-4e10-a8f2-f0228bf6d460/je/2000xxs.jpg`,
+      secondary: [
+        `https://img.jamesedition.com/listing_images/2021/09/21/17/16/55/4c265442-651b-4725-86b7-285ad81373bb/je/2000xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2021/09/21/17/16/55/c5b1b38c-c256-4f2d-b971-bfe0d7eec600/je/2000xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2021/09/21/17/16/55/ac7625af-f7c5-4b6a-acf5-e4e49ea01539/je/2000xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2021/09/21/17/16/55/4ac961b1-34a5-4b18-9283-263971e69bd5/je/2000xxs.jpg`,
+      ],
+    },
+  },
+  {
+    id: 5,
+    category: `collectibles`,
+    owner: {
+      name: `Kim Jong Unga-Bunga`,
+      country: `North Korea`,
+    },
+    item: {
+      name: `Jesus On The Cross Pendant`,
+      price: `14,664`,
+      year: 1966,
+      place: `Secret Place, North Korea`,
+      about: `This Jesus on the Cross Pendant is crafted in 14ct Yellow Gold and is accentuated with the application of 133 round brilliant cut diamonds pave set with a total weight of 0.44ct it is SI1 in clarity and E/F in colour. Beautifully crafted by hand, this pendant weighs a total of 17.40 grams.`,
+    },
+    images: {
+      main: `https://img.jamesedition.com/listing_images/2021/03/19/16/58/56/b43a73b3-37a6-4053-a415-49f380b839b6/je/1040x620xc.jpg`,
+      secondary: [
+        `https://img.jamesedition.com/listing_images/2021/03/19/16/58/57/ef5db26d-c478-4fc9-85f2-98db196ab870/je/355x210xc.jpg`,
+        `https://img.jamesedition.com/listing_images/2021/03/19/16/58/56/5712ec69-1ac2-4de7-9889-21ea1494bdf8/je/355x210xc.jpg`,
+        `https://img.jamesedition.com/listing_images/2021/03/19/16/58/57/ef5db26d-c478-4fc9-85f2-98db196ab870/je/355x210xc.jpg`,
+        `https://img.jamesedition.com/listing_images/2021/03/19/16/58/56/5712ec69-1ac2-4de7-9889-21ea1494bdf8/je/355x210xc.jpg`,
+      ],
+    },
+  },
+  {
+    id: 6,
+    category: `yachts`,
+    owner: {
+      name: `Joe Hidden`,
+      country: `Turkey`,
+    },
+    item: {
+      name: `Alpha Spritz 116`,
+      price: `2,944,924`,
+      year: 2013,
+      place: `Turkey`,
+      about: `Alpha worked with Italian superyacht designer Giorgia Cassetta and naval architect Laurent Giles to create a platform for family fun; elegant and crafted with care. According to Cassetta, the yacht has the feel of a luxury villa combined with great functionality for long-range cruising and comfort.`,
+    },
+    images: {
+      main: `https://img.jamesedition.com/listing_images/2022/08/24/15/29/58/08c8bd6d-2af6-4d61-987e-542f98a57a78/je/2000xxs.jpg`,
+      secondary: [
+        `https://img.jamesedition.com/listing_images/2022/08/24/15/29/58/a4cc2d91-e996-49d4-aeaa-bbe9d0e11d80/je/2000xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2022/08/24/15/29/58/8d25ef79-15df-4632-8d16-3eefcad09fdb/je/2000xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2022/08/24/15/29/58/9bda9d98-4db8-44db-82b9-7764621f53a5/je/2000xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2022/08/24/15/29/58/17808dfc-ae6a-4228-b274-bfdf800658ab/je/2000xxs.jpg`,
+      ],
+    },
+  },
+  {
+    id: 7,
+    category: `yachts`,
+    owner: {
+      name: `Selmon Bhoi`,
+      country: `Turkey`,
+    },
+    item: {
+      name: ` Alpha Alfresco 110`,
+      price: `3,320,893`,
+      year: 2014,
+      place: `Turkey`,
+      about: `Excellent opportunity to own an Alpha Custom Yacht. Alpha Custom Yachts is a company that helps all the ocean lovers sail away with a smile over and over again. With their obsession to quality and detail, you get the feeling you are standing at water level with the big windows all throughout the vessel.`,
+    },
+    images: {
+      main: `https://img.jamesedition.com/listing_images/2022/08/24/15/29/58/60e37d78-0c4e-484d-bc5c-dbe883c9913b/je/1040x620xc.jpg`,
+      secondary: [
+        `https://img.jamesedition.com/listing_images/2022/08/24/15/29/58/6e996e10-fa2f-4515-88cc-baad202225cd/je/2000xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2022/08/24/15/29/58/c7b2b204-b6ce-4594-9190-3af29d535fe7/je/2000xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2022/08/24/15/29/58/3f2e80b1-0034-4739-953f-33e773d03d1c/je/2000xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2022/08/24/15/29/58/771dbd9b-667a-4f28-9241-d7b1299cd97b/je/2000xxs.jpg`,
+      ],
+    },
+  },
+  {
+    id: 8,
+    category: `bikes`,
+    owner: {
+      name: `Donald Trumpet`,
+      country: `France`,
+    },
+    item: {
+      name: `Triumph "Green Legend" Steve McQueen Edition`,
+      price: `1,987,712`,
+      year: 2015,
+      place: `Chauvigny, France`,
+      about: `It is based on Triumph Bonneville as the workshop decided to Manufacturing, Design, Achieve its last custom.`,
+    },
+    images: {
+      main: `https://img.jamesedition.com/listing_images/2019/04/26/16/47/24/6af2cb14-35bf-4b8a-a8ec-7baabd93de24/je/2000xxs.jpg`,
+      secondary: [
+        `https://img.jamesedition.com/listing_images/2019/05/02/14/17/49/03869c2f-c11b-44f3-82c0-843f9f540e9c/je/2000xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2019/05/02/14/37/35/c0d1f31c-def3-46fb-8da8-bc6dcfb12815/je/2000xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2019/04/26/16/47/23/1abb5750-62d2-4ab8-86a0-563784b746ca/je/2000xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2019/05/04/13/19/47/b9f502eb-992c-44cc-85e9-0ff4c83d676f/je/2000xxs.jpg`,
+      ],
+    },
+  },
+  {
+    id: 9,
+    category: `watches`,
+    owner: {
+      name: `Bill Doors`,
+      country: `Hong Kong`,
+    },
+    item: {
+      name: `Richard Mille RM 029`,
+      price: `20,098,875`,
+      year: 2018,
+      place: `Hong Kong`,
+      about: `2018 Paper, Mint Condition Watch with Box and Paper Full Set!!`,
+    },
+    images: {
+      main: `https://img.jamesedition.com/listing_images/2022/08/24/15/29/54/c610148e-7cd0-4f42-abe5-5c4df2fa3475/je/2000xxs.jpg`,
+      secondary: [
+        `https://img.jamesedition.com/listing_images/2022/08/24/15/29/55/76e41e2e-04c9-4eee-81d3-2c7cf5d9b6b9/je/2000xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2022/08/24/15/29/55/3e7c1237-aec2-4dcc-8bcf-8e484b2f2295/je/2000xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2022/08/24/15/29/55/b2cfd53f-8e9b-4d91-ab4e-84f7bffe8336/je/2000xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2022/08/24/15/29/55/8bafe823-a473-4d53-99ef-db408b003899/je/2000xxs.jpg`,
+      ],
+    },
+  },
+  {
+    id: 10,
+    category: `jets`,
+    owner: {
+      name: `Barak Omama`,
+      country: `France`,
+    },
+    item: {
+      name: `Pilatus PC12`,
+      price: `306,766,286`,
+      year: 2011,
+      place: `France`,
+      about: `Executive Platinum 6+2-Seat interior. 8-Seat Oxygen System and 2 Standard Seats. Executive 6-Seat interior. Platinum Upgrade 6-Seat.`,
+    },
+    images: {
+      main: `https://img.jamesedition.com/listing_images/2021/06/16/13/48/00/6761e81d-b3bd-4648-90d6-ac01998f3071/je/2000xxs.jpg`,
+      secondary: [
+        `https://img.jamesedition.com/listing_images/2021/06/16/13/48/00/58509724-dc15-437a-bcf0-59e99e3c257d/je/2000xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2021/06/16/13/48/00/075dec2a-9bee-4916-9d47-433c6eb27fb1/je/2000xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2021/06/16/13/48/00/58509724-dc15-437a-bcf0-59e99e3c257d/je/2000xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2021/06/16/13/48/00/7d20e245-4e25-4a11-a166-b8cc31854713/je/2000xxs.jpg`,
+      ],
+    },
+  },
+  {
+    id: 11,
+    category: `jewelry`,
+    owner: {
+      name: `Cliff Steele`,
+      country: `Australia`,
+    },
+    item: {
+      name: `Tennis Chain`,
+      price: `69,420`,
+      year: 2021,
+      place: ` Melbourne, Victoria, Australia `,
+      about: `This stunning Tennis Chain is complete in 14k Yellow Gold featuring 62 round brilliant diamonds, 16.19ct in combined weight. The Round Brilliant diamonds are VS2 clarity and H colour, completed in a 4 claw setting. The necklace is a total length of 57cm with a tongue and groove safety clasp. `,
+    },
+    images: {
+      main: `https://img.jamesedition.com/listing_images/2021/05/04/11/26/23/83c2c73a-456a-457d-9089-24d7026e6d36/je/1040x620xc.jpg`,
+      secondary: [
+        `https://img.jamesedition.com/listing_images/2021/05/04/11/26/23/71ec7faa-ce20-43df-838f-542880b6d101/je/355x210xc.jpg`,
+        `https://img.jamesedition.com/listing_images/2021/05/04/11/26/23/358825a8-c8ba-4f56-b82a-793fbc2dd4df/je/355x210xc.jpg`,
+        `https://img.jamesedition.com/listing_images/2021/05/04/11/26/23/71ec7faa-ce20-43df-838f-542880b6d101/je/355x210xc.jpg`,
+        `https://img.jamesedition.com/listing_images/2021/05/04/11/26/23/358825a8-c8ba-4f56-b82a-793fbc2dd4df/je/355x210xc.jpg`,
+      ],
+    },
+  },
+  {
+    id: 12,
+    category: `cars`,
+    owner: {
+      name: `King Elizabet`,
+      country: `United States`,
+    },
+    item: {
+      name: `Lamborghini Huracan`,
+      price: `20,393,982`,
+      year: 2017,
+      place: `Bellevue, WA, United States`,
+      about: `This build starts with the most desirable white exterior on black/red interior configuration with front axle lift system, upgraded factory wheels, carbon ceramic disk brakes, full alcantara interior, carbon fiber, and additional factory options.`,
+    },
+    images: {
+      main: `https://img.jamesedition.com/listing_images/2022/06/07/15/32/10/c7530899-c422-4676-809e-496ded5dca1a/je/1100xxs.jpg`,
+      secondary: [
+        `https://img.jamesedition.com/listing_images/2022/06/07/15/32/10/363688bb-da60-4f1b-a513-ecf582e676fc/je/1100xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2022/06/07/15/32/10/3277d5a1-c216-437e-bbe3-0222686abca2/je/1100xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2022/06/07/15/32/10/70b5db9d-0291-4df5-9f3d-7716da40f9ee/je/1100xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2022/06/07/15/32/10/3b18ada1-7b1d-4f3a-8ca4-384fc0ba688d/je/1100xxs.jpg`,
+      ],
+    },
+  },
+  {
+    id: 13,
+    category: `cars`,
+    owner: {
+      name: `Mark Cucumberg`,
+      country: `United States`,
+    },
+    item: {
+      name: `Aston Martin Vantage`,
+      price: `10,994,981`,
+      year: 2020,
+      place: `Beverly Hills, CA, United States`,
+      about: `O'Gara Beverly Hills is pleased to present for sale this stunning 2020 Aston Martin Vantage as part of our hand-selected pre-owned lineup. This gorgeous example is finished in the Stratus White exterior paint and Copper Tan. Other dealer added factory options include: RWD ZF 8-Speed Automatic V8.`,
+    },
+    images: {
+      main: `https://img.jamesedition.com/listing_images/2022/08/18/16/45/09/3bd16f8b-124c-4bc0-b179-e1de7ed880f6/je/1100xxs.jpg`,
+      secondary: [
+        `https://img.jamesedition.com/listing_images/2022/08/18/16/45/09/f486ca8e-9109-4e37-9c43-0c43ca1e1200/je/1100xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2022/08/18/16/45/09/9fd1829e-8144-4eb7-a0aa-59e4b35378d0/je/1100xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2022/08/18/16/45/09/a4585327-2b57-42ad-ad7b-fc9a0e8910d1/je/1100xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2022/08/18/16/45/09/109173e0-545f-4a0a-aa23-a987467bec4f/je/1100xxs.jpg`,
+      ],
+    },
+  },
+  {
+    id: 14,
+    category: `helicopters`,
+    owner: {
+      name: `Justout Beiber`,
+      country: `Austria`,
+    },
+    item: {
+      name: `Eurocopter AS355F2`,
+      price: `62,946,848`,
+      year: 1991,
+      place: `Austria`,
+      about: `The aircraft is operating, why the condition and component times will be subject to changes depending upon such operations. Verification of the condition of the aircraft and its component times shall be the responsibility of the purchaser.`,
+    },
+    images: {
+      main: `https://img.jamesedition.com/listing_images/2021/02/05/09/32/53/9930e7e1-9af9-43e5-8467-4ed3d1990665/je/1040x620xc.jpg`,
+      secondary: [
+        `https://img.jamesedition.com/listing_images/2021/11/22/16/19/16/c53f183e-d476-45bf-bd57-d78a03d232c1/je/355x210xc.jpg`,
+        `https://img.jamesedition.com/listing_images/2021/11/22/16/19/16/efbd6407-e9d8-4f0b-a74b-25ba8097329e/je/355x210xc.jpg`,
+        `https://img.jamesedition.com/listing_images/2021/02/05/09/32/53/0cb486db-d7c1-4c75-8574-29004449edf4/je/355x210xc.jpg`,
+        `https://img.jamesedition.com/listing_images/2021/02/05/09/32/53/bad5cb44-9199-4841-ae8c-347a51aac7b3/je/355x210xc.jpg`,
+      ],
+    },
+  },
+  {
+    id: 15,
+    category: `jets`,
+    owner: {
+      name: `Ariana Grinder`,
+      country: `Germany`,
+    },
+    item: {
+      name: `TurboProp`,
+      price: `71,805,101`,
+      year: 2013,
+      place: `Neustadt an der Aisch, Bavaria, Germany`,
+      about: `After the first 500 we had listed sold within days for the full asking price, we are pleased to be able to bring to market a second aircraft, and this time with no fewer then 450 hours total time from new. The aircraft presents like new and is in outstanding condition. With a Engine TBO of 3500 hours you have over 3000 hours left to fly before overhaul.`,
+    },
+    images: {
+      main: `https://img.jamesedition.com/listing_images/2022/05/02/10/52/47/ff83d638-e552-4df7-937d-8263c025cf8c/je/2000xxs.jpg`,
+      secondary: [
+        `https://img.jamesedition.com/listing_images/2022/05/02/10/52/46/6c30c2e0-a9f6-4d6a-9488-286f16fba897/je/2000xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2022/05/02/10/52/46/04fb0f44-5e53-4ed2-8a53-4467e7e55725/je/2000xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2022/05/02/10/52/46/6c30c2e0-a9f6-4d6a-9488-286f16fba897/je/2000xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2022/05/02/10/52/46/04fb0f44-5e53-4ed2-8a53-4467e7e55725/je/2000xxs.jpg`,
+      ],
+    },
+  },
+  {
+    id: 16,
+    category: `collectibles`,
+    owner: {
+      name: `Selena Bromez`,
+      country: `United Kingdom`,
+    },
+    item: {
+      name: `Classic Flugel Matt Black and Red Piano`,
+      price: `4,427,057`,
+      year: 1986,
+      place: `United Kingdom`,
+      about: `The black and red matt finish flugel is a show stopping piano! With a vibrant red interior and integrated self play it really makes a statement in the home and provides hours of entertainment. Complete with matching stool. `,
+    },
+    images: {
+      main: `https://img.jamesedition.com/listing_images/2021/10/11/10/35/10/6fdc3884-7011-4d95-ae4c-577b8df7abbf/je/2000xxs.jpg`,
+      secondary: [
+        `https://img.jamesedition.com/listing_images/2021/10/11/10/35/09/92f48fd9-62e9-44bd-987f-403b014aac29/je/2000xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2021/10/11/10/35/09/fd654068-af42-46cf-bf75-bea08d932359/je/2000xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2021/10/11/10/35/09/48418c49-a945-49b9-be9e-5d3247bc4b9f/je/2000xxs.jpg`,
+        `https://img.jamesedition.com/listing_images/2021/10/11/10/35/09/9c9174da-d531-4447-a494-4d713178e365/je/2000xxs.jpg`,
+      ],
+    },
+  },
+];
