4b42ae79ab73122c766e61bf5ea051301c8ae27c
vipul-khedekar <3kvipul@gmail.com>
2022-09-06 20:15:53 +0530

    Update: Page not found with return to home or store button.

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+
 function Error() {
-  return <div>Error</div>;
+  return (
+    <div className="h-full w-full flex flex-col justify-center items-center gap-8 mt-32">
+      <h1 className="text-semiDarkWhite text-4xl">
+        Whoopse!!! Such page doesn't exists.
+      </h1>
+      <Link
+        className="bg-gradient-to-br from-darkGold to-gold rounded-lg py-1 px-4"
+        to="/store/buy"
+      >
+        Return to Store
+      </Link>
+    </div>
+  );
 }
 
 export default Error;
