f826598a019e2256b3cd6f33d48760a0e789146b
vipul-khedekar <3kvipul@gmail.com>
2022-09-07 21:04:52 +0530

    Update: Stylized seller details and actions.

--- src/components/SellerDetails.tsx
+++ src/components/SellerDetails.tsx
@@ -1,5 +1,37 @@
-function SellerDetails() {
-  return <div>SellerDetails</div>;
+import { StoreItems } from "../data/StoreItems";
+
+type ImageGridProps = {
+  id: number;
+};
+
+function SellerDetails({ id }: ImageGridProps) {
+  const product = StoreItems.find((item) => {
+    return item.id === Number(id);
+  });
+
+  return (
+    <article className="w-[380px] flex flex-col justify-between items-center gap-4 p-4 border-[2.5px] border-gold rounded-md text-sm">
+      <p>Seller: {product.owner.name}</p>
+
+      <p>{product.owner.country}</p>
+
+      <div className="flex gap-4">
+        <button
+          className="w-[140px] bg-gradient-to-br from-darkGold to-gold py-2 px-3 rounded-md"
+          type="button"
+        >
+          Can we talk?
+        </button>
+
+        <button
+          className="w-[140px] bg-gradient-to-br from-darkGold to-gold py-2 px-3 rounded-md"
+          type="button"
+        >
+          Take my money
+        </button>
+      </div>
+    </article>
+  );
 }
 
 export default SellerDetails;
